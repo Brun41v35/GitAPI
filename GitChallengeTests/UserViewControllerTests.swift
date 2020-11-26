6ece4b52241cfb6c8b05b4a97b888fbe5f1dacf4
Bruno Alves <alves.bruno945@gmail.com>
2020-11-26 12:50:41 -0300

    Implementando testes na classe usuario

--- GitChallengeTests/UserViewControllerTests.swift
+++ GitChallengeTests/UserViewControllerTests.swift
@@ -9,14 +9,47 @@ import XCTest
 @testable import GitChallenge
 
 class UserViewControllerTests: XCTestCase {
-
+    
+    var user: UserViewController!
+    
     override func setUpWithError() throws {
-    }
-
-    override func tearDownWithError() throws {
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        user = storyboard.instantiateViewController(withIdentifier: "userView") as? UserViewController
+        user.loadViewIfNeeded()
     }
     
-    func testValidaSeCampoTemValor() {
+    override func tearDownWithError() throws {
+        user.loadViewIfNeeded()
+    }
+    
+    func testValidaSeTextFieldEstaConectado() {
+        do {
+            let userTextField = try XCTUnwrap(user.tfNameUser, "Olha, o tfNameUser não está conectado!")
+        } catch {
+            print(error)
+        }
+    }
+    
+    func testValidaSeCampoTemEntradaCorreta() {
+        do {
+            let userTextField = try XCTUnwrap(user.tfNameUser, "Olha, o tfNameUser não está conectado!")
+            XCTAssertEqual(userTextField.textContentType, UITextContentType.name, "tfNameUser não está com o content type nome")
+        } catch {
+            print(error)
+        }
+    }
+    
+    func testValidaSeBotaoTemAcao() {
         
+        let getUsersButton: UIButton = user.getUsers
+        XCTAssertNotNil(getUsersButton, "User View Controller não tem proprieda UIButton")
+        
+        guard let getUsersActions = getUsersButton.actions(forTarget: user, forControlEvent: .touchUpInside) else {
+        XCTFail("UIButton does not have actions assigned for Control Event .touchUpInside")
+        return
+        }
+        
+        XCTAssertTrue(getUsersActions.contains("salvarNome:"))
     }
 }
